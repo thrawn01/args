ba1a0dcdd218418405edc9ba4f4e271c87dec40f
Derrick J. Wippler <thrawn01@gmail.com>
2016-03-08 15:36:07 -0600

    Added etcd-demo, brain storming new etcd integrations

---
+++ examples/etcd-demo.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	etcd "github.com/coreos/etcd/client"
+	"github.com/thrawn01/args"
+)
+
+func main() {
+
+	// Create the parser with program name 'example'
+	// and environment variables prefixed with APP_
+	parser := args.Parser(args.Name("example"), args.EtcdPath("/exampleApp"))
+
+	// Since 'Etcd()' is not used, this option is not configurable via etcd
+	parser.Opt("--etcd-endpoints").Alias("-eP").IsSlice()
+	Default("192.168.5.1,192.168.5.2").Help("List of etcd endpoints")
+
+	// Define a name used by other services to discover this service
+	parser.Opt("--service-name").Alias("-sN")
+	Default("frontend1").Help("Name used for service discovery")
+
+	// if EtcdKey() is not given etcd keys are crafted by using
+	// the name of the option. This etcd key will be '/exampleApp/message'
+	parser.Opt("--message").Etcd().
+		Default("over-ten-thousand").Help("send a message")
+
+	// Defines --power-level command line option, but defines the
+	// etcd key as '/exampleApp/powerLevel'
+	parser.Opt("--power-level").EtcdKey("powerLevel").
+		Default("10000").Help("set our power level")
+
+	// Config options can also be used
+	parser.Conf("config-version").IsInt().Etcd().
+		Help("Indicates updates to etcd are complete and we should reload the service")
+
+	// You can also use groups with etcd keys
+	db = parser.Group("database")
+
+	// etcd key will be '/exampleApp/database/host'
+	db.Conf("host").Default("localhost").Etcd().Help("database hostname")
+	// etcd key will be '/exampleApp/database/debug'
+	db.Conf("debug").IsTrue().Etcd().Help("enable database debug")
+	// etcd key will be '/exampleApp/database/database'
+	db.Conf("database").IsString().Etcd().Default("myDatabase").Help("name of database to use")
+	// etcd key will be '/exampleApp/database/user'
+	db.Conf("user").Etcd().Help("database user")
+	// etcd key will be '/exampleApp/database/pass'
+	db.Conf("pass").Etcd().Help("database password")
+
+	opts, err := parser.ParseArgs(nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+
+	cfg := etcd.Config{
+		Endpoints: opts.Slice("etcd-endpoints"),
+		Transport: etcd.DefaultTransport,
+		// set timeout per request to fail fast when the target endpoint is unavailable
+		HeaderTimeoutPerRequest: time.Second,
+	}
+
+	client, err := etcd.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	keysAPI := etcd.NewKeysAPI(client)
+
+	// Grab values for config options defined with 'Etcd()'
+	opt, err := parser.FromEtcd(keysAPI, log)
+	if err != nil {
+		fmt.Printf("Etcd or value parse issue - %s\n", err.Error())
+	}
+
+	// Complex Example, where the config changes in etcd do
+	// not get applied until 'config-version' is changed.
+	stagedConf := args.NewGroupOptions()
+	// Watch etcd for any configuration changes
+	args.WatchEtcd(parser.EtcdPath(), func(group, key, value string) {
+		// Apply all the config
+		stagedConf.Get(group).Set(key, value)
+		if group == "" && key == "config-version" {
+			// NOTE: If you are using opt.ThreadSafe() you can safely
+			// ignore the 'opt' returned by Apply(). This is because Apply()
+			// will update it's internal pointer to the new version of the
+			// config and subsequent calls to opt.ThreadSafe() will always
+			// safely return the new version of the config
+
+			// Apply the new config to the parser
+			opt, err := parser.Apply(stagedConf)
+			if err != nil {
+				fmt.Printf("Probably a type cast error - %s\n", err.Error())
+				return
+			}
+			// Clear the staged config values
+			stagedConf = args.NewGroupOptions()
+			fmt.Printf("Config has been updated to version %d\n", opt.Int("config-version"))
+		}
+	})
+}
--- examples/main.go
+++ examples/main.go
@@ -12,13 +12,15 @@ type Config struct {
 	Message    string
 	Slice      []string
 	Verbose    int
+	DbHost     string
 }
 
 func main() {
 	var conf Config
 
-	// Create the parser
-	parser := args.Parser(args.Name("example"))
+	// Create the parser with program name 'example'
+	// and environment variables prefixed with APP_
+	parser := args.Parser(args.Name("example"), args.EnvPrefix("APP_"))
 
 	parser.Opt("--power-level").StoreInt(&conf.PowerLevel).
 		Env("POWER_LEVEL").Default("10000").Help("set our power level")
@@ -27,16 +29,20 @@ func main() {
 	parser.Opt("--power-level").Alias("-p").StoreInt(&conf.PowerLevel).
 		Env("POWER_LEVEL").Default("10000").Help("set our power level")
 
-	// Options can begin with -name, --name or even ++name. Most alpha non word character are supported
-	parser.Opt("++power").Alias("+p").IsString().Default("11,000").Help("prefix demo")
+	// Options can begin with -name, --name or even ++name.
+	// Most non word characters are supported
+	parser.Opt("++config-file").Alias("+c").IsString().
+		Default("/path/to/config").Help("path to config file")
 
 	// Use the args.Env() function to define an environment variable
+	// NOTE: Since the parser was passed args.EnvPrefix("APP_") the actual
+	// environment variable name is 'APP_MESSAGE'
 	parser.Opt("--message").Alias("-m").StoreStr(&conf.Message).
 		Env("MESSAGE").Default("over-ten-thousand").Help("send a message")
 
 	// Pass a comma separated list of strings and get a []string
 	parser.Opt("--slice").Alias("-s").StoreSlice(&conf.Slice).Env("LIST").
-		Default("one,two,three").Help("list of messages"))
+		Default("one,two,three").Help("list of messages")
 
 	// Count the number of times an argument is seen
 	parser.Opt("--verbose").Alias("-v").Count().StoreInt(&conf.Verbose).Help("be verbose")
@@ -46,6 +52,28 @@ func main() {
 
 	// Specify the type of the arg with IsInt(), IsString(), IsBool() or IsTrue()
 	parser.Opt("--help").Alias("-h").IsTrue().Help("show this help message")
+
+	// 'Conf' options are not set via the command line but can be set
+	// via a config file or an environment variable
+	parser.Conf("twelve-factor").Env("TWELVE_FACTOR").Help("Demo of config options")
+
+	// Define a 'database' subgroup
+	db = parser.Group("database")
+	// Add command line Options to the subgroup
+	db.Opt("--host").Alias("-dH").StoreStr(&conf.DbHost).
+		Default("localhost").Help("database hostname")
+
+	// Add subgroup specific config. 'Conf' options are not set via the
+	// command line but can be set via a config file or anything that calls parser.Apply()
+	db.Conf("debug").IsTrue().Help("enable database debug")
+
+	// 'Conf' option names are not allowed to start with a non word character like
+	// '--' or '++' so they can not be confused with command line options
+	db.Conf("database").IsString().Default("myDatabase").Help("name of database to use")
+
+	// If no type 'IsString', 'IsInt' is specified, defaults to 'IsString'
+	db.Conf("user").Help("database user")
+	db.Conf("pass").Help("database password")
 
 	// Pass our own argument list, or nil to parse os.Args[]
 	opt, err := parser.ParseArgs(nil)
@@ -60,6 +88,14 @@ func main() {
 	fmt.Printf("CAST Slice     '%s'\n", opt.Slice("slice"))
 	fmt.Printf("CAST Verbose   '%d'\n", opt.Int("verbose"))
 	fmt.Printf("CAST Debug     '%t'\n", opt.Bool("debug"))
+
+	// Fetch Group values
+	dbOpt := opt.Group("database")
+	fmt.Printf("CAST DB Host   '%s'\n", dbOpt.String("host"))
+	fmt.Printf("CAST DB Debug  '%t'\n", dbOpt.Bool("debug"))
+	fmt.Printf("CAST DB User   '%s'\n", dbOpt.String("user"))
+	fmt.Printf("CAST DB Pass   '%s'\n", dbOpt.String("pass"))
+
 	fmt.Println("")
 
 	// Values can also be stored in a struct
@@ -68,18 +104,25 @@ func main() {
 	fmt.Printf("STRUCT Slice   '%s'\n", conf.Slice)
 	fmt.Printf("STRUCT Verbose '%d'\n", conf.Verbose)
 	fmt.Printf("STRUCT Debug   '%t'\n", opt.Bool("debug"))
+	fmt.Printf("STRUCT DbHost  '%s'\n", conf.DbHost)
 	fmt.Println("")
 
 	iniFile := []byte(`
 		power-level=20000
-		message=OVER-THEN-THOUSAND!
+		message=OVER-TEN-THOUSAND!
 		slice=three,four,five,six
 		verbose=5
 		debug=true
-    `)
+
+		[database]
+		debug=false
+		host=mysql.thrawn01.org
+		user=my-username
+		pass=my-password
+	`)
 
 	// Make configuration simply by reading arguments from an INI file
-	opt, err = parser.ParseIni(iniFile)
+	opt, err = parser.FromIni(iniFile)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(-1)
@@ -99,4 +142,63 @@ func main() {
 		parser.PrintHelp()
 		os.Exit(-1)
 	}
+
+	// Thread safe access to the options are available, calls to ThreadSafe()
+	// ensure no data race occurs if the config is updated outside of the go routine
+	go func() {
+		// Use ThreadSafe() inside http.Handler to get race free access to your config
+		localOpt := opt.ThreadSafe()
+		fmt.Printf("THREAD SAFE DBHostname '%s'\n", localOpt.Group("database").String("host"))
+		fmt.Printf("THREAD SAFE Verbose '%d'\n", localOpt.String("verbose"))
+	}()
+
+	// Update the config
+	updatedIniFile := []byte(`
+		verbose=0
+
+		[database]
+		host=postgresql.thrawn01.org
+	`)
+	opt, err = parser.FromIni(updatedIniFile)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+	fmt.Printf("UPDATED CONFIG DBHostname '%s'\n", opt.Group("database").String("host"))
+	fmt.Printf("UPDATED CONFIG Verbose '%d'\n", opt.String("verbose"))
+
+	// Simple example
+	args.WatchFile(opt.String("config-file"), func() {
+		opt, err = parser.FromIni(opt.String("config-file"))
+		if err != nil {
+			fmt.Printf("Failed to update config - %s\n", err.Error())
+			return
+		}
+	})
+
+	// Complex Example
+	args.WatchFile(opt.String("config-file"), func() {
+		rawValues, err = parser.ParseIni(opt.String("config-file"))
+		if err != nil {
+			fmt.Printf("Failed to update config - %s\n", err.Error())
+			return
+		}
+		// Apply these raw values to the parser rules
+		opt, err = parser.Apply(rawValues)
+		if err != nil {
+			fmt.Printf("Probably a type cast error - %s\n", err.Error())
+			return
+		}
+		// Compare the parsed file with our current values
+		changedKeys := opt.Compare(rawValues)
+		// Iterate through all the keys that changed
+		for key := range changedKeys {
+			// If the database host changed
+			if key == "database:host" {
+				// Re-init the database connection
+				initDb(opt)
+			}
+		}
+
+	})
 }
